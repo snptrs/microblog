b46d35508b2d897d9965cbe0853c3f47ec8b5483
Sean Peters <sean@snptrs.dev>
2024-07-29 21:19:06 +0100

    Change URL format

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -3,6 +3,7 @@ module.exports = {
 	layout: "layouts/post.njk",
 	permalink: function ({ title, page }) {
 		const slug = title ? this.slugify(title) : this.slugify(page.fileSlug);
-		return `/${slug}/`;
+		const date = new Date(page.date);
+		return `/${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, "0")}/${slug}/`;
 	},
 };
